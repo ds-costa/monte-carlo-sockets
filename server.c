1af6559b8f61c3c67bac8bef8ca372890ef20933
Murilo Carvalho <48266939+murilo-oak@users.noreply.github.com>
2021-05-02 16:39:37 -0300

    Feature/create listener socket (#4)
    * feature(socketconfig.c): configure socket
    
    configure the basics of a socket
    
    * refactor(socketconfig.h): delete suplicated code
    
    * refactor(socketconfig.h): delete some comments
    
    * refactor: move definitions to lib
    
    ipv4, tcp, listen_quenue_lengh, port and max_clients

--- server.c
+++ server.c
@@ -3,8 +3,12 @@
 #include <math.h>
 #include "src/socketconfig.h"
 
-
 int main(int argc, char **argv) {
+    //socketdata_t sock;
+
+    //sock = scof_create_default_connection_type();
+    scof_activate_listener_mode(&sock);
+    
     printf("Hello Server...\n");
     return 0;
 }
--- src/socketconfig.h
+++ src/socketconfig.h
@@ -4,18 +4,35 @@
 #define IPV4 AF_INET
 #define TCP SOCK_STREAM
 #define LISTEN_QUENUE_LENGTH 16
+#define PORT 5000
 
 #define MAX_CLIENTS 16
 
-#include <stdbool.h>
-#include <sys/socket.h> 
-#include <netinet/in.h>
-
 typedef struct {
+    //[!] need to add structure to keep following the client 
     struct sockaddr_in address;
     int file_descriptor;
     int address_size;
-    bool option_name;
+    int option_name;
 } socketdata_t;
+
+/**
+ * function: scof_get_address_host
+ * Return a socket address (IPV4, PORT, INADDR_ANY)
+ */
+struct sockaddr_in scof_get_address_host();
+
+/**
+ * function: scof_create_default_connection_type
+ * Create a socket with the default configuration
+ */
+socketdata_t scof_create_default_connection_type();
+
+/**
+ * function: scof_activate_listener_mode
+ * Creates, set the option, bind, define listen quenue lenght of listener socket
+ * @param sock, Socket struct with all information 
+ */
+int scof_activate_listener_mode(socketdata_t* sock);
 
 #endif
