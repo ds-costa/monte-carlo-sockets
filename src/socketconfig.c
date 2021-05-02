b51a476c84a05849aed110abba6e94adc621d79d
murilo-oak <murilo63793@gmail.com>
2021-05-02 15:51:50 -0300

    feature(socketconfig.c): configure socket
    configure the basics of a socket

--- src/socketconfig.c
+++ src/socketconfig.c
@@ -1,1 +1,80 @@
 #include "socketconfig.h"
+
+#define IPV4 AF_INET
+#define TCP SOCK_STREAM
+#define LISTEN_QUENUE_LENGTH 16
+#define PORT 5000
+
+#define MAX_CLIENTS 16
+
+#include <stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <strings.h>
+#include <errno.h>
+
+
+/**
+ * function: scof_get_address_host
+ * Return a socket address (IPV4, PORT, INADDR_ANY)
+ */
+struct sockaddr_in scof_get_address_host() {
+    struct sockaddr_in server_addr;
+
+    server_addr.sin_family = IPV4;
+    server_addr.sin_port = htons(PORT); // host-endian to network-endian
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(server_addr.sin_zero),8);
+
+    return server_addr;
+}
+
+/**
+ * function: scof_create_default_connection_type
+ * Create a socket with the default configuration
+ */
+socketdata_t scof_create_default_connection_type(){
+    socketdata_t sock;
+    
+    sock.file_descriptor = -1;
+    sock.option_name = true;
+    sock.address = scof_get_address_host();
+
+    return sock;
+}
+
+/**
+ * function: scof_activate_listener_mode
+ * Creates, set the option, bind, define listen quenue lenght of listener socket
+ * @param sock, Socket struct with all information 
+ */
+int scof_activate_listener_mode(socketdata_t* sock) {
+    int socket_file_descriptor;
+
+    socket_file_descriptor = socket(IPV4, TCP, 0);
+    
+    if(socket_file_descriptor == -1) {
+        perror("[-]Error creating Socket\n");
+        exit(1);
+    }
+
+    if(setsockopt(socket_file_descriptor, SOL_SOCKET, SO_REUSEADDR, &(sock->option_name), sizeof(sock->option_name)) == -1){
+        perror("[-]Error in Setsockopt\n");
+        exit(1);
+    }
+
+    if(bind(socket_file_descriptor, (struct sockaddr *) &(sock->address), sizeof(struct sockaddr))){
+        perror("[-]It was not possible to bind\n");
+        exit(1);
+    }
+
+    if (listen(socket_file_descriptor, LISTEN_QUENUE_LENGTH) == -1) {
+        perror("[-]Error defining input queue size\n");
+        exit(1);
+    }
+
+    printf("\n[+]TCP Server created! Waiting client at port: %d\n", PORT);
+    return socket_file_descriptor;
+}
--- src/socketconfig.h
+++ src/socketconfig.h
@@ -4,18 +4,45 @@
 #define IPV4 AF_INET
 #define TCP SOCK_STREAM
 #define LISTEN_QUENUE_LENGTH 16
+#define PORT 5000
 
 #define MAX_CLIENTS 16
 
+#include <stdio.h>
+#include<stdlib.h>
 #include <stdbool.h>
 #include <sys/socket.h> 
 #include <netinet/in.h>
+#include <strings.h>
+#include <errno.h>
 
 typedef struct {
+    //[!] ainda precisa adicionar informação para ter controle de x clientes
+    //[!] no código do thiago leal fica na struct SocketSet
+    //[!] não adicionei pq a gente pode fazer diferente
     struct sockaddr_in address;
     int file_descriptor;
     int address_size;
-    bool option_name;
+    int option_name;
 } socketdata_t;
+
+/**
+ * function: scof_get_address_host
+ * Writes to the array the (int) content read in the stdin
+ */
+struct sockaddr_in scof_get_address_host();
+
+/**
+ * function: scof_create_default_connection_type
+ * Writes to the array the (int) content read in the stdin
+ */
+socketdata_t scof_create_default_connection_type();
+
+/**
+ * function: scof_activate_listener_mode
+ * Creates, set the option, bind, define listen quenue lenght of listener socket
+ * @param sock, Socket struct with all information 
+ */
+int scof_activate_listener_mode(socketdata_t* sock);
 
 #endif
