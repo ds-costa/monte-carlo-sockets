4643807794ac2b47b94d0e98221ebc6c00ae4308
ds-costa <davidshcosta@gmail.com>
2021-05-10 19:08:50 -0300

    feat: montecarlo lib
    .c and .h files

--- standalone.c
+++ standalone.c
@@ -8,19 +8,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include "src/montecarlo.h"
 
-// Random numbers seed
-#define SEED time(NULL)
-
-// Return a random number in [lo, hi] interval (inclusive)
-#define RANDOM_DOUBLE(lo, hi) lo + ((double) rand() / (double) (RAND_MAX) * (hi - lo))
-
-/**
- * function: monte_carlo_pi
- * Monte Carlo PI computation method implementation
- * @param total_points, Number of random points to be generated
- */
-double monte_carlo_pi(unsigned long total_points);
 
 int main(int argc, char **argv) {
     
@@ -34,22 +23,4 @@ int main(int argc, char **argv) {
     printf("monte_carlo> Error: %g\n", fabs(M_PI - pi));
 
     return 0;
-}
-
-double monte_carlo_pi(unsigned long total_points) {
-    
-    unsigned long points_inside_circle = 0;
-    unsigned long i;
-    double x, y;
-
-    for(i = 0; i < total_points; i++) {
-        x = RANDOM_DOUBLE(0, 1);
-        y = RANDOM_DOUBLE(0, 1);
-
-        if((x * x) + (y * y) <= 1.0) {
-            points_inside_circle++;
-        }
-    }
-
-    return 4.0 * ((double) points_inside_circle / (double) total_points);
 }
