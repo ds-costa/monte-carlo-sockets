3440e841e322382cc2ab84cd00762f461ad44457
murilo-oak <murilo63793@gmail.com>
2021-05-21 17:04:30 -0300

    feat(server.c): clients delive their results to server
    server sums all clients results

--- server.c
+++ server.c
@@ -2,64 +2,116 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "src/socketconfig.h"
 #include "src/convert.h"
 #include "src/data.h"
 
-int main(int argc, char **argv) {
- 
-    socketdata_t server_socket, client_socket;    
-    int number_of_clients = 5;
-    int i;
-    unsigned long number_points = 5000000000;
-    struct sockaddr_in newAddr;
-    socklen_t addr_size;
-    int clients[number_of_clients];
-    pipe_t clients_pipes[number_of_clients];
-
-    server_socket = sc_new_socket_data_server();
-    sc_activate_listener_mode(&server_socket);
+void handle_tcp_connection(unsigned long number_points, char* buffer, int file_descriptor) { 
+    printf("[+]Handle TCP client\n");
     
+    conv_ulong_2_string(number_points, buffer);
+
+    data_send(file_descriptor, buffer);
+    data_receive(file_descriptor, buffer);
+}
+
+double wait_and_sum_clients_results(int number_of_clients, pipe_t* clients_pipes, char* buffer) {
+    double sum;
+    int i;
+
+    for(i = 0; i < number_of_clients; i++) {
+        read(clients_pipes[i][0], buffer, sizeof(buffer)); //wait and read clients' result
+        printf("%lf\n", conv_string_2_double(buffer));
+        sum += conv_string_2_double(buffer); 
+    }
+    return sum;
+}
+
+void close_all_tcp_handlers(int number_of_clients, int (*clients)[number_of_clients]) {
+    int i;
+
+    for(i = 0; i < number_of_clients; i++) { //close all sockets tcp handlers
+        close(*(clients[i]));
+    }
+}
+
+void close_read_mode_pipes(int number_of_clients, pipe_t (*clients_pipes)[2]) {
+    int i;
+
+    for(i = 0; i < number_of_clients; i++) { //close all pipes
+        close(*(clients_pipes[i][0]));
+    }
+}
+
+void initilize_and_connect_clients(socketdata_t* server_socket, int number_of_clients, int *clients, pipe_t* clients_pipes) {
+    int i;
+    struct sockaddr_in newAddr;
+    socklen_t addr_size = sizeof(newAddr);  
+
     for(i = 0; i < number_of_clients; i++) {
         system("./client &");
+        
         clients[i] = accept(
-            server_socket.file_descriptor, 
+            server_socket->file_descriptor, 
             (struct sockaddr *)&newAddr, 
-            &addr_size
+            &addr_size 
         );
         pipe(clients_pipes[i]);
 
         printf("[+]Receiving connection (Client: %s , Port: %d)\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
     }
+}
 
-    char *test_buffer[] = {
-        "Brazil",
-        "Japan",
-        "EUA",
-        "Germany",
-        "Bahia"
-    };
+int main(int argc, char **argv) {
+ 
+    socketdata_t server_socket;      
+    
+    int number_of_clients = 4;
+    unsigned long number_points = 1000000;
+
+    int clients[number_of_clients];
+    pipe_t clients_pipes[number_of_clients];
+
+    //aux variables
+    int i;
+    double sum = 0.0;
+    
+    char buffer[1014];
+
+    pid_t child_pid;
+
+    server_socket = sc_new_socket_data_server();
+    sc_activate_listener_mode(&server_socket);
+    
+    initilize_and_connect_clients(&server_socket, number_of_clients, clients, clients_pipes);
 
     for(i = 0; i < number_of_clients; i++) {
-        data_send(clients[i], test_buffer[i]);
+        if((child_pid = fork()) < 0) {
+            perror("[-]Fork error\n");
+        }
+        else if(child_pid != 0) { //parent
+            close(clients_pipes[i][1]); //read only
+        }
+        else { //child
+            close(clients_pipes[i][0]); //writing only
+            handle_tcp_connection(number_points, buffer, clients[i]);
+            
+            write(clients_pipes[i][1], &buffer, strlen(buffer)+1); //writes client's result in pipe
+            close(clients_pipes[i][1]);
+            exit(0);
+        }
     }
 
-    // unsigned int child_process_counter = 0;
-    // pid_t child_pid;
+    sum = wait_and_sum_clients_results(number_of_clients, clients_pipes, buffer);
+    sum /= number_of_clients;
 
-    // while(true) {
-    //     child_pid = fork();
-    //     if(child_pid < 0) {
-    //         perror("[-]Fork error\n");
-    //         return 0;
-    //     }
-    //     else if(child_pid == 0) {
-    //         //Handle tcp
-    //         printf("[+]Handle TCP client\n");
-    //         exit(0);
-    //     }
-    // }
-    close(server_socket.file_descriptor);
+    printf("\nPI: %.10lf\n", sum);
+    
+    close_all_tcp_handlers(number_of_clients, &clients);
+    close_read_mode_pipes(number_of_clients, &clients_pipes);
 
+    close(server_socket.file_descriptor); //close parent server process
+    
     return 0;
 }
