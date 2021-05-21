0b280db3119aec82278d50a69a6d2dfb3a1792ac
murilo-oak <murilo63793@gmail.com>
2021-05-21 17:05:58 -0300

    feat(client.c): client computes pi and send to server

--- client.c
+++ client.c
@@ -4,12 +4,14 @@
 #include "src/socketconfig.h"
 #include "src/convert.h"
 #include "src/data.h"
+#include "src/montecarlo.h"
 
 int main(int argc, char **argv) {
-
     socketdata_t client_socket;
     char buffer[MAX_BUFFER_LENGTH];
+    double pi;
 
+    srand(SEED);
     client_socket = sc_new_socket_data_client();
 
     sc_establish_client_connection(&client_socket);
@@ -17,6 +19,13 @@ int main(int argc, char **argv) {
     data_receive(client_socket.file_descriptor, buffer);
 
     printf("client(%d)> %s\n", client_socket.file_descriptor, buffer);
+    
+    //monte carlo
+    //conv_double_2_string(pi, buffer);
+    pi = monte_carlo_pi(conv_string_2_double(buffer));
+    conv_double_2_string(pi, buffer);
+
+    data_send(client_socket.file_descriptor, buffer);
 
     close(client_socket.file_descriptor);
     return 0;
