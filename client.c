d78cb3289e5a4961ee2d4c154cf26cc7d385f0c5
murilo-oak <murilo63793@gmail.com>
2021-05-24 16:00:31 -0300

    feat(client.c): add different seed to each client
    clients seeds uses their file descriptor as parameter

--- client.c
+++ client.c
@@ -11,11 +11,10 @@ int main(int argc, char **argv) {
     char buffer[MAX_BUFFER_LENGTH];
     double pi;
 
-    srand(SEED);
     client_socket = sc_new_socket_data_client();
-
     sc_establish_client_connection(&client_socket);
 
+    srand(SEED + client_socket.file_descriptor * 100000); //making each client have different seeds
     data_receive(client_socket.file_descriptor, buffer);
 
     printf("client(%d)> %s\n", client_socket.file_descriptor, buffer);
